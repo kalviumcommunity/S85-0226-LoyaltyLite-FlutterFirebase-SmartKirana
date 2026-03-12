2022d990877c056d858d72034300d076b9022975
Rajeev-Foryou <mandalrajeev3@gmail.com>
2026-03-12 17:54:04 +0530

    feat: connected Flutter app with Firebase backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   if (Firebase.apps.isEmpty) {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    await Firebase.initializeApp();
+    debugPrint('Firebase has been successfully initialized!');
   }
-  
+
   runApp(const SmartKiranaApp());
 }
 
